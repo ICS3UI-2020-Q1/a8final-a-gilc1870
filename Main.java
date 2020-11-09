d1449ee1e53b6db6c14866cda4912d7bd1c7f3bc
gilc1870 <gilc1870@wrdsb.ca>
2020-11-09 03:28:22 +0000

    Counts down from 10 and stops at 0

--- Main.java
+++ Main.java
@@ -6,12 +6,14 @@ public class Main implements Runnable, ActionListener{
 
   // Class Variables  
   
-
+JTextField field;
+JButton button;
+JButton secondbutton;
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Count down");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
@@ -19,16 +21,78 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
+    JPanel mainPanel = new JPanel();
+
     
 
+    
+
+button = new JButton("Count down");
+field = new JTextField("10");
+secondbutton = new JButton("reset");
+
+
+mainPanel.add(button);
+mainPanel.add(field);
+mainPanel.add(secondbutton);
+ 
+ field.setEnabled(false);
+
+ 
+ button.addActionListener(this);
+ button.setActionCommand("Count Down");
+ 
+
+ 
+ secondbutton.addActionListener(this);
+ secondbutton.setActionCommand("reset");
+ 
+
+
+
+
+
+
+
+
+
+
+frame.add(mainPanel);
+
+
+
+ 
   }
 
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
     // get the command from the action
-    String command = e.getActionCommand();
-
+     String command = e.getActionCommand();
+    // For counter
+    if(command.equals("Count Down")){
+  
+String text = field.getText();
+int fieldNum = Integer.parseInt(text);
+if(fieldNum > 0){
+fieldNum = fieldNum - 1;
+field.setText("" + fieldNum);
+        
+    }
   }
+
+
+if(command.equals("reset")){
+  String text = field.getText();
+int fieldNum = Integer.parseInt(text);
+    field.setText("10");
+      
+  
+      }
+    }
+
+
+
+  
 
   // Main method to start our program
   public static void main(String[] args){
